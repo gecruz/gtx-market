ae4789196d386cdd1c410efe0577fec8524b98a0
Gustavo Silva <gecruz.dev@gmail.com>
2022-08-23 14:31:39 -0300

    feat: add angular material module

---
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+  ]
+})
+export class AngularMaterialModule { }
